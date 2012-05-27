3affbe30e8b30874860de20271eef78dfd65cd7a
Robert Moore <me@robdmoore.id.au>
2012-05-27 12:16:50 +0800

    Added the rest of the tests for ControllerExtensions class

--- TerseControllerTesting.Tests/ControllerExtensionsTests.cs
+++ TerseControllerTesting.Tests/ControllerExtensionsTests.cs
@@ -19,6 +19,27 @@ namespace TerseControllerTesting.Tests
         {
             _controller.WithModelErrors();
             Assert.That(_controller.ModelState.IsValid, Is.False);
-        } 
+        }
+
+        [Test]
+        public void Call_action()
+        {
+            _controller.WithCallTo(c => c.SomeAction());
+            Assert.That(_controller.SomeActionCalled);
+        }
+
+        [Test]
+        public void Call_child_action()
+        {
+            _controller.WithCallToChild(c => c.SomeChildAction());
+            Assert.That(_controller.SomeChildActionCalled);
+        }
+
+        [Test]
+        public void Throw_exception_for_child_action_call_to_non_child_action()
+        {
+            var exception = Assert.Throws<InvalidControllerActionException>(() => _controller.WithCallToChild(c => c.SomeAction()));
+            Assert.That(exception.Message, Is.EqualTo("Expected action SomeAction of controller TerseControllerTesting.Tests.TestControllers.TestController to be a child action, but it didn't have the ChildActionOnly attribute."));
+        }
     }
 }
--- TerseControllerTesting.Tests/TestControllers/TestController.cs
+++ TerseControllerTesting.Tests/TestControllers/TestController.cs
@@ -2,5 +2,22 @@
 
 namespace TerseControllerTesting.Tests.TestControllers
 {
-    class TestController : Controller {}
+    class TestController : Controller
+    {
+        public bool SomeActionCalled { get; set; }
+        public bool SomeChildActionCalled { get; set; }
+
+        public ActionResult SomeAction()
+        {
+            SomeActionCalled = true;
+            return new EmptyResult();
+        }
+
+        [ChildActionOnly]
+        public ActionResult SomeChildAction()
+        {
+            SomeChildActionCalled = true;
+            return new EmptyResult();
+        }
+    }
 }
