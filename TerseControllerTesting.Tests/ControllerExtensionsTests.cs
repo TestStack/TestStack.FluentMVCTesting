e877bc5065efc3cc61eeb6a201f828c4e8feb130
Robert Moore <me@robdmoore.id.au>
2012-05-27 12:08:42 +0800

    Added first test

---
+++ TerseControllerTesting.Tests/ControllerExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using TerseControllerTesting.Tests.TestControllers;
+
+namespace TerseControllerTesting.Tests
+{
+    [TestFixture]
+    class ControllerExtensionsShould
+    {
+        private TestController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _controller = new TestController();
+        }
+
+        [Test]
+        public void Give_controller_modelstate_errors()
+        {
+            _controller.WithModelErrors();
+            Assert.That(_controller.ModelState.IsValid, Is.False);
+        } 
+    }
+}
---
+++ TerseControllerTesting.Tests/TestControllers/TestController.cs
@@ -0,0 +1,6 @@
+﻿using System.Web.Mvc;
+
+namespace TerseControllerTesting.Tests.TestControllers
+{
+    class TestController : Controller {}
+}
