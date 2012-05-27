39e18c5e2b52229c620c71356e8a690ecf809a2a
Robert Moore <me@robdmoore.id.au>
2012-05-27 17:43:52 +0800

    Added tests for ModelTest

---
+++ TerseControllerTesting.Tests/ModelTestTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+
+namespace TerseControllerTesting.Tests
+{
+    [TestFixture]
+    class ModelTestShould
+    {
+        private ModelTest<TestViewModel> _modelTest;
+        private ViewTestController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _controller = new ViewTestController();
+            _modelTest = new ModelTest<TestViewModel>(_controller);
+        }
+
+        [Test]
+        public void Check_for_lack_of_model_errors()
+        {
+            _modelTest.AndNoModelErrors();
+        }
+
+        [Test]
+        public void Check_for_unexpected_model_errors()
+        {
+            _controller.ModelState.AddModelError("key", "error");
+            var exception = Assert.Throws<ViewResultModelAssertionException>(() =>
+                _modelTest.AndNoModelErrors()
+            );
+            Assert.That(exception.Message, Is.EqualTo("Expected controller 'ViewTestController' to have no model errors, but it had some."));
+        }
+
+        [Test]
+        public void Check_for_model_error_in_key()
+        {
+            _controller.ModelState.AddModelError("Key", "error");
+            _modelTest.AndModelError("Key");
+        }
+
+        [Test]
+        public void Check_for_unexpected_lack_of_model_error_in_key()
+        {
+            var exception = Assert.Throws<ViewResultModelAssertionException>(() =>
+                _modelTest.AndModelError("Key")
+            );
+            Assert.That(exception.Message, Is.EqualTo("Expected controller 'ViewTestController' to have a model error against key 'Key', but none found."));
+        }
+
+        [Test]
+        public void Check_for_model_error_in_property()
+        {
+            _controller.ModelState.AddModelError("Property1", "error");
+            _modelTest.AndModelErrorFor(m => m.Property1);
+        }
+
+        [Test]
+        public void Check_for_execpected_lack_of_model_error_in_property()
+        {
+            var exception = Assert.Throws<ViewResultModelAssertionException>(() =>
+                _modelTest.AndModelErrorFor(m => m.Property1)
+            );
+            Assert.That(exception.Message, Is.EqualTo("Expected controller 'ViewTestController' to have a model error for member 'Property1', but none found."));
+        }
+    }
+}
--- TerseControllerTesting/ModelTest.cs
+++ TerseControllerTesting/ModelTest.cs
@@ -17,21 +17,21 @@ namespace TerseControllerTesting
         {
             var member = ((MemberExpression)memberWithError.Body).Member.Name;
             if (!_controller.ModelState.ContainsKey(member) || _controller.ModelState[member].Errors.Count == 0)
-                throw new ViewResultModelAssertionException(string.Format("Expected controller '{0}' to have a model error for member '{1}', but none found.", _controller, member));
+                throw new ViewResultModelAssertionException(string.Format("Expected controller '{0}' to have a model error for member '{1}', but none found.", _controller.GetType().Name, member));
             return new ModelErrorTest<TModel>(this, member, _controller.ModelState[member].Errors);
         }
 
         public ModelErrorTest<TModel> AndModelError(string errorKey)
         {
             if (!_controller.ModelState.ContainsKey(errorKey) || _controller.ModelState[errorKey].Errors.Count == 0)
-                throw new ViewResultModelAssertionException(string.Format("Expected controller '{0}' to have a model error against key '{1}', but none found.", _controller, errorKey));
+                throw new ViewResultModelAssertionException(string.Format("Expected controller '{0}' to have a model error against key '{1}', but none found.", _controller.GetType().Name, errorKey));
             return new ModelErrorTest<TModel>(this, errorKey, _controller.ModelState[errorKey].Errors);
         }
 
         public void AndNoModelErrors()
         {
             if (!_controller.ModelState.IsValid)
-                throw new ViewResultModelAssertionException(string.Format("Expected controller '{0}' to have no model errors, but it had some.", _controller));
+                throw new ViewResultModelAssertionException(string.Format("Expected controller '{0}' to have no model errors, but it had some.", _controller.GetType().Name));
         }
     }
 }
