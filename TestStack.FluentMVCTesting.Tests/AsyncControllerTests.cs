0152b5c787f2334b4c03bc51cba700f2e36b7df1
Robert Moore <me@robdmoore.id.au>
2013-05-16 17:49:44 +0800

    Added support for async controller actions

---
+++ TestStack.FluentMVCTesting.Tests/AsyncControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using TestStack.FluentMVCTesting.Tests.TestControllers;
+
+namespace TestStack.FluentMVCTesting.Tests
+{
+    class AsyncControllersShould
+    {
+        private AsyncController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _controller = new AsyncController();
+        }
+
+        [Test]
+        public void Work_correctly_for_valid_check()
+        {
+            _controller.WithCallTo(c => c.AsyncViewAction())
+                .ShouldRenderDefaultView();
+        }
+
+        [Test]
+        public void Work_correctly_for_invalid_check()
+        {
+            Assert.Throws<ActionResultAssertionException>(
+                () => _controller.WithCallTo(c => c.AsyncViewAction()).ShouldGiveHttpStatus()
+            );
+        }
+        [Test]
+        public void Work_correctly_for_valid_child_action_check()
+        {
+            _controller.WithCallToChild(c => c.AsyncChildViewAction())
+                .ShouldRenderDefaultView();
+        }
+
+        [Test]
+        public void Work_correctly_for_invalid_child_action_check()
+        {
+            Assert.Throws<InvalidControllerActionException>(
+                () => _controller.WithCallToChild(c => c.AsyncViewAction())
+            );
+        }
+    }
+}
