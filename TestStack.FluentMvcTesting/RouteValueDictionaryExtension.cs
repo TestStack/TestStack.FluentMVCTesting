c55d859a93e5ce914e0bf54a3654b1e0c0bdd005
michal-franc <michal.franc@live.com>
2014-02-06 23:29:00 +0000

    Implementation of extensions for fluent RouteValueDictionary assertions

---
+++ TestStack.FluentMvcTesting/RouteValueDictionaryExtension.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Web.Routing;
+
+namespace TestStack.FluentMVCTesting
+{
+    public static class RouteValueDictionaryExtension
+    {
+        public static RouteValueDictionary WithRouteValue(this RouteValueDictionary dict, string key)
+        {
+            if (!dict.ContainsKey(key))
+            {
+                throw new MissingRouteValueException(string.Format("No value {0} in the route dictionary", key));
+            }
+
+            return dict;
+        }
+
+        public static RouteValueDictionary WithRouteValue<T>(this RouteValueDictionary dict, string key, T value)
+        {
+            dict.WithRouteValue(key);
+
+            var actualValue = dict[key];
+
+            if (!(actualValue is T))
+            {
+                throw new ValueTypeMismatchException(string.Format("Invalid type of Value with key {0} \r\n expected {1} \r\n but got {2}", key, value.GetType(), actualValue.GetType()));
+            }
+
+            if (!Equals(actualValue, value))
+            {
+                throw new InvalidRouteValueException(string.Format("Invalid value for key {0} \r\n expected {1} \r\n but got {2} in the route dictionary",key ,value, actualValue));
+            }
+
+            return dict;
+        }
+    }
+}
