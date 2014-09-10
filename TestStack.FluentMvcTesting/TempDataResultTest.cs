c70054263efdb76d2b873399500db4190296c7b2
ByteBlast <byteblast@gmail.com>
2014-09-10 17:22:28 +0100

    Made implementation adhere to DRY.

--- TestStack.FluentMvcTesting/TempDataResultTest.cs
+++ TestStack.FluentMvcTesting/TempDataResultTest.cs
@@ -14,46 +14,13 @@ namespace TestStack.FluentMVCTesting
 
         public TempDataResultTest AndShouldHaveTempDataProperty(string key, object value = null)
         {
-            var actual = _controller.TempData[key];
-
-            if (actual == null)
-            {
-                throw new TempDataAssertionException(string.Format(
-                    "Expected TempData to have a non-null value with key \"{0}\", but none found.", key));
-            }
-
-            if (value != null && actual.GetType() != value.GetType())
-            {
-                throw new TempDataAssertionException(string.Format(
-                    "Expected value to be of type {0}, but instead was {1}.",
-                    value.GetType().FullName,
-                    actual.GetType().FullName));
-            }
-
-            if (value != null && !value.Equals(actual))
-            {
-                throw new TempDataAssertionException(string.Format(
-                    "Expected value for key \"{0}\" to be \"{1}\", but instead found \"{2}\"", key, value, actual));
-            }
-
+            _controller.ShouldHaveTempDataProperty(key, value);
             return this;
         }
 
         public TempDataResultTest AndShouldHaveTempDataProperty<TValue>(string key, Func<TValue, bool> predicate)
         {
-            var actual = _controller.TempData[key];
-
-            if (actual == null)
-            {
-                throw new TempDataAssertionException(string.Format(
-                    "Expected TempData to have a non-null value with key \"{0}\", but none found.", key));
-            }
-
-            if (!predicate((TValue)actual))
-            {
-                throw new TempDataAssertionException("Expected view model to pass the given condition, but it failed.");
-            }
-
+            _controller.ShouldHaveTempDataProperty(key, predicate);
             return this;
         }
     }
